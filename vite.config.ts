620e1a74524f761a60f3c555a0bf618215367205
huynhductruong <khungking12345@gmail.com>
2025-04-19 10:19:35 +0700

    chore(ITS_RCT): fix deploy download

--- vite.config.ts
+++ vite.config.ts
@@ -1,50 +1,35 @@
-import { defineConfig } from 'vitest/config';
-import react from '@vitejs/plugin-react';
+/// <reference types="vitest" />
+
+import react from '@vitejs/plugin-react-swc';
+import { defineConfig } from 'vite';
 import checker from 'vite-plugin-checker';
-import dts from 'vite-plugin-dts';
+import webfontDownload from 'vite-plugin-webfont-dl';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
+    webfontDownload(),
     tsconfigPaths(),
-    dts({
-      insertTypesEntry: true,
-      tsconfigPath: './tsconfig.app.json',
-      rollupTypes: true,
-    }),
-    !!process.env.DEV &&
+    !process.env.VITEST &&
       checker({
         typescript: true,
-        eslint: { lintCommand: "eslint 'src/**/*.ts' 'src/**/*.tsx'", useFlatConfig: true },
+        eslint: {
+          lintCommand: "eslint 'src/**/*.ts' 'src/**/*.tsx'",
+          useFlatConfig: true,
+        },
       }),
   ],
-  build: {
-    sourcemap: true,
-    assetsDir: './src/assets',
-    rollupOptions: {
-      external: ['react', 'react-dom'],
-      input: 'public/index.html',
-      output: {
-        globals: {
-          react: 'React',
-          'react-dom': 'ReactDOM',
-        },
-      },
-    },
-  },
-  css: {
-    modules: {
-      localsConvention: 'camelCase', // Optional: Use camelCase for class names
-    },
-  },
   test: {
     environment: 'jsdom',
     setupFiles: './vitest.prepare.ts',
+    include: ['./src/**/*.spec.tsx'],
     coverage: {
-      reportsDirectory: './tests/unit/coverage',
-      provider: 'istanbul',
+      provider: 'v8',
+      reporter: ['text', 'json'],
+      reportsDirectory: './test/coverage',
+      extension: ['.ts', '.tsx'],
     },
   },
 });
